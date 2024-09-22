4ccefde7a5185c00180ce49e99d35d96ccf8b39b
Christoph Euskirchen <mail@christopheuskirchen.de>
2024-09-22 14:16:53 +0200

    Login logic

---
+++ src/app/core/models/user-dto-auth.model.ts
@@ -0,0 +1,4 @@
+export interface UserDtoAuthModel {
+  mail: string;
+  password: string;
+}
--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../../core/services/auth.service";
+import {Router} from "@angular/router";
+import {UserDtoAuthModel} from "../../../core/models/user-dto-auth.model";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['../../../core/stylesheets/formula.css']
 })
-export class LoginComponent {
 
+
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string | undefined;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      mail: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const userDtoAuth: UserDtoAuthModel = {
+        mail: this.loginForm.value.mail,
+        password: this.loginForm.value.password
+      };
+
+      this.authService.login(userDtoAuth).subscribe(
+        (response) => {
+          console.log('Login successful:', response);
+          this.router.navigate(['/']); // navigate to home or dashboard after login
+        },
+        (error) => {
+          console.error('Login failed:', error);
+          this.errorMessage = 'Login failed. Please check your credentials.';
+        }
+      );
+    } else {
+      this.errorMessage = 'Please fill in all fields correctly!';
+    }
+  }
 }
