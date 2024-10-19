75285e26f5fe275a66532c44e1f968865b950e94
Christoph Euskirchen <mail@christopheuskirchen.de>
2024-10-19 09:55:33 +0200

    New module frame module implementation

--- src/app/shared/components/quill-config.ts
+++ src/app/shared/components/quill-config.ts
@@ -4,6 +4,7 @@ export const quillModules: QuillModules = {
   toolbar: [
     ['bold', 'italic', 'underline'],
     [{ 'header': 1}],
+    [{ 'script': 'super' }, { 'script': 'sub' }],
     [{ 'list': 'ordered' }, { 'list': 'bullet' }],
     [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
     [{ 'color': [] }, { 'background': [] }], // Dropdown with colors
